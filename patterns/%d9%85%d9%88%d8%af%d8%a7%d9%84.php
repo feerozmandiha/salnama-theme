f527520362b17c1fd82bce622570bafce1d6a152
feerozmandiha@gmail.com <feerozmandiha@gmail.com>
2025-11-03 13:52:33 +0330

    بروز رسانی پا برگ

---
+++ patterns/%d9%85%d9%88%d8%af%d8%a7%d9%84.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: مودال
+ * Slug: salnama-theme/%d9%85%d9%88%d8%af%d8%a7%d9%84
+ * Categories: مودال‌های سالنامه
+ */
+?>
+<!-- wp:group {"className":"modal-header","layout":{"type":"constrained"}} -->
+<div class="wp-block-group modal-header"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">راه ارتباطی برای مشاوره</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">کارشناسان با تجربه ما آماده پاسخ گویی به سوالات شما هستند.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>تماس مستقیم با کارشناسان</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center"><a href="tel:09124533878">09124533878</a></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>ارتباط با شبکه های اجتماعی</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"feerozmandiha@gmail.com","service":"mail"} /-->
+
+<!-- wp:social-link {"url":"https://wa.me/+989124533878","service":"whatsapp"} /-->
+
+<!-- wp:social-link {"url":"https://t.me/salnama","service":"telegram"} /-->
+
+<!-- wp:social-link {"url":"https://instagram.com/salnamanoo","service":"instagram"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph -->
+<p>جهت تماس کارشناسان ما لطفا فرم زیر را تکمیل کنید تا در سیریع ترین زمان با شما تماس بگیرند.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
