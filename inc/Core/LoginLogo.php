94eef5ffd24ee61c8921c4f8a7c07643bbca04d0
feerozmandiha@gmail.com <feerozmandiha@gmail.com>
2025-11-03 12:50:08 +0330

    بروز رسانی لوگو صفحه ورود

--- inc/Core/LoginLogo.php
+++ inc/Core/LoginLogo.php
@@ -14,34 +14,57 @@ class LoginLogo {
     public function add_custom_logo_style() {
         $logo_url = $this->get_site_logo_url();
 
-        if ( ! $logo_url ) {
-            return; // اگر لوگو تنظیم نشده باشد، هیچ کاری نکن
+        if (!$logo_url) {
+            return;
         }
 
-        echo '<style type="text/css">
-            .login h1 a {
-                background-image: url("' . esc_url($logo_url) . '");
-                background-size: contain;
-                background-repeat: no-repeat;
-                width: 100%;
-                height: 100px;
-                display: block;
-            }
-        </style>';
+        wp_add_inline_style(
+            'login',
+            sprintf(
+                '.login h1 a {
+                    background-image: url("%s");
+                    background-size: contain;
+                    background-repeat: no-repeat;
+                    width: 100%%;
+                    height: 100px;
+                    display: block;
+                }',
+                esc_url($logo_url)
+            )
+        );
     }
 
     /**
      * Returns the site logo URL from custom_logo setting.
      */
     private function get_site_logo_url(): ?string {
-        $custom_logo_id = get_theme_mod('custom_logo');
-        if ( ! $custom_logo_id ) {
-            return null;
+        // اول سعی کن لوگوی سایت را از طریق site_logo بگیر
+        $site_logo_id = get_theme_mod('site_logo');
+        
+        // اگر site_logo وجود نداشت، از custom_logo استفاده کن
+        if (!$site_logo_id) {
+            $site_logo_id = get_theme_mod('custom_logo');
+        }
+        
+        // اگر لوگوی سایت پیدا شد
+        if ($site_logo_id) {
+            $logo_data = wp_get_attachment_image_src($site_logo_id, 'full');
+            if (!empty($logo_data[0])) {
+                return $logo_data[0];
+            }
         }
 
-        $logo_data = wp_get_attachment_image_src($custom_logo_id, 'full');
-        return $logo_data[0] ?? null;
+        // اگر لوگوی سایت وجود نداشت، از آیکون سایت استفاده کن
+        $icon_url = get_site_icon_url();
+        if ($icon_url) {
+            return $icon_url;
+        }
+
+        // در نهایت تصویر پیش‌فرض
+        return null; // بهتر است null برگردانی تا استایل اضافه نشود
     }
+
+
 
     public function change_logo_url() {
         return home_url();
